42f49ebdc0697c2c2f164914cc2a17e572cb1d1c
Milena Sviatek <porstudag@gmail.com>
2022-06-28 19:17:13 -0300

    segundo commit

--- ex79.py
+++ ex79.py
@@ -1,9 +1,11 @@
 lista = list()
 while True:
     num = int(input('Digite um valor: '))
-    res = str(input('Deseja continuar?'))
     if num not in lista:
         lista.append(num)
+    else:
+        print('Valor duplicado - não adicionado...')
+    res = str(input('Deseja continuar?'))
     while res.capitalize().split()[0] != 'S' and res.capitalize().split()[0]  != 'N':
         res = str(input('\033[0;31mResposta inválida\033[m'))
     if res.capitalize().split()[0] == 'N':
